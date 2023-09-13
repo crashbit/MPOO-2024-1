849ffd30ce692651bc7e6a7a350c3d3516342ce0
Germán Santos <crashbito@gmail.com>
2023-09-13 16:13:51 -0600

    Create optionals.swift

---
+++ optionals.swift
@@ -0,0 +1,42 @@
+// NIL
+// NULL = void *
+
+// int *x;
+// x = NULL
+
+var nombre:String?
+nombre = nil
+nombre = "Pedro"
+
+    // 1era forma
+//if let contenido = nombre{
+//    print(contenido)
+//}else{
+//    print("Solo hay nulos")
+//}
+
+    // 2da forma
+//var contenido = nombre ?? "Sin nombre"
+//print(contenido)
+
+// 3era forma
+//func evaluaNombre( nombre:String?){
+//    guard let nombre = nombre else{return} 
+//    print(nombre)
+//}
+//
+//evaluaNombre(nombre: nombre)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
